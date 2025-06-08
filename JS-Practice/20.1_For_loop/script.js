0c4ecfcc5db7e0cfd4d51b733500c7d7bdb535c5
YashwanthVelupula777 <yashwanthvelupula286@gmail.com>
2025-06-08 09:13:26 +0530

    added arrow functions and few other scripts

--- JS-Practice/20.1_For_loop/script.js
+++ JS-Practice/20.1_For_loop/script.js
@@ -1,1 +1,36 @@
-console.log('hey 123');
+// for loop contains 3 expressions. Initialisation, condition and final expression.
+
+for (let i = 0; i < 5; i++) {
+      console.log(i); // Output: 0, 1, 2, 3, 4
+    }
+
+// using lrngth property gor reverse number iteration. 
+    for (let i = 5; i > 0; i--) {
+      console.log(i); // Output: 5, 4, 3, 2, 1
+    }    
+
+// iterating through an Array.
+const fruits = ["apple", "banana", "cherry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// for in loop
+const data = {Name: "Yash", age: 28, height : 186, city: "hyd" }
+for (let key in data) {
+    console.log(key + ":" + data[key]);
+}
+
+
+// Arrow function 
+//Arrow function without arguments
+const sayHello = () => "Hello, World!";
+console.log(sayHello());
+// Output will be : Hello World!
+
+// Arrow function with one argument 
+const square = x => x * x;
+console.log(square(23)); 
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Git commands.
+1) <Git pull> # it will take latest changes from git.
+2) <git status> # check any modified or any added files .
+3) <git add .> # Staigning all modified or and newly added files.
+4) <git commit -m "your own message ">
+5) <git push> last stpe tp push changes to actuly git setver.
